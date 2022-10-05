be0b451efb34dd6be2643eb0d8efdb7447408921
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-05 18:14:31 +0300

    Expand functionality to take user input and play one round

--- script.js
+++ script.js
@@ -26,9 +26,21 @@ function playRound(playerSelection, computerSelection) {
         return "You lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
         return "You win! Rock beats Scissors";
-    } else return;
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "paper"){
+        return "It\'s a tie!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! Scissors beat Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors beat Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
+        return "It\'s a tie!";
+    } else return;    
 }
-
-const playerSelection = "rock";
+const playerSelectionRaw = window.prompt("Rock, paper or scissors?");
+const playerSelection = playerSelectionRaw.toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
