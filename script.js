a5b650f62ed42238fd84719e27cb82235d2bf8d8
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-05 17:24:32 +0300

    Add function that generates computers choice randomly

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+/* Rock Paper Scissors
+Game is played in the browsers console against the computer.
+When user gives string input of either "rock", "paper" or "scissors",
+then game compares user input to computers choice,
+then game logs result to console.*/
+
+//Function that randomly returns a computers choice
+
+function getComputerChoice(){
+    let selector = Math.floor(Math.random() * 3) + 1; //makes 1 or 2 or 3 randomly
+    if (selector === 1){
+        return "Rock";
+    } else if (selector === 2){
+        return "Paper";
+    } else if (selector === 3){
+        return "Scissors";
+    } else return;
+}
+
