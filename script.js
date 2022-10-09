75371b769498d7e859611090d07eeccac290cd17
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-09 13:52:19 +0300

    Add simple UI

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-/* Rock Paper Scissors
-Game is played in the browsers console against the computer.
-When user gives string input of either "rock", "paper" or "scissors",
-then game compares user input to computers choice,
-then game logs result to console.*/
-
-//Function that randomly returns a computers choice
-
+//Rock Paper Scissors
+//Variables to hold scores
 let playerScore = 0;
 let computerScore = 0;
 
+//Variables to hold DOM elements
+const buttons = document.querySelectorAll('button');
+const container = document.querySelector('.container');
+const score = document.querySelector('.score');
+const announcement = document.querySelector('.announcement');
+
+//Function which runs game on mouse click and gives output
+buttons.forEach((button) => {
+    button.addEventListener('click', function (e){
+        playRound(e.target.id, getComputerChoice());
+        
+        score.textContent = `Player\'s score: ${playerScore} vs 
+                             Computer\'s score: ${computerScore}`; //Score counter div
+        
+        if (playerScore === 5){
+            announcement.textContent = "YOU WIN!"; //Gameover conditions
+            buttons.forEach((button) => {button.disabled = true});
+        } else if (computerScore === 5){
+            announcement.textContent = "YOU LOSE!";
+            buttons.forEach((button) => {button.disabled = true});
+        } return;
+    });
+});
+
+//Calculate computers choice - rock or paper or scissors
 function getComputerChoice(){
     let selector = Math.floor(Math.random() * 3) + 1; //makes 1 or 2 or 3 randomly
     if (selector === 1){
@@ -19,69 +38,49 @@ function getComputerChoice(){
         return "scissors";
     } else return;
 }
-
-//Function that plays a single round of rock paper scissors
-
+//Function that plays a single round, contains all of games logic
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "rock"){
         console.log("It\'s a tie!");
+        container.textContent = "It\'s a tie!";
         return;
     } else if (playerSelection === "rock" && computerSelection === "paper"){
         console.log("You lose! Paper beats Rock");
+        container.textContent = "You lose! Paper beats Rock"; 
         computerScore++;
         return;
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
         console.log("You win! Rock beats Scissors");
+        container.textContent = "You win! Rock beats Scissors";
         playerScore++;
         return;
     } else if (playerSelection === "paper" && computerSelection === "rock"){
         console.log("You win! Paper beats Rock");
+        container.textContent = "You win! Paper beats Rock";
         playerScore++;
         return;
     } else if (playerSelection === "paper" && computerSelection === "paper"){
         console.log("It\'s a tie!");
+        container.textContent = "It\'s a tie!";
         return;
     } else if (playerSelection === "paper" && computerSelection === "scissors"){
         console.log("You lose! Scissors beat Paper");
+        container.textContent = "You lose! Scissors beat Paper";
         computerScore++;
         return;
     } else if (playerSelection === "scissors" && computerSelection === "rock"){
         console.log("You lose! Rock beats Scissors");
+        container.textContent = "You lose! Rock beats Scissors";
         computerScore++;
         return;
     } else if (playerSelection === "scissors" && computerSelection === "paper"){
         console.log("You win! Scissors beat Paper");
+        container.textContent = "You win! Scissors beat Paper";
         playerScore++;
         return;
     } else if (playerSelection === "scissors" && computerSelection === "scissors"){
         console.log("It\'s a tie!");
+        container.textContent = "It\'s a tie!";
         return;
-    } else return "Enter only one of the following words: rock, paper, scissors!";    
+    } return;    
 }
-//Function to play a 5 round game
-
-function game(){
-
-    for (let i = 1; i < 6; i++){
-        const playerSelectionRaw = window.prompt("Rock, paper or scissors?");
-        const playerSelection = playerSelectionRaw.toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(`Round Nr.${i}:`);
-        playRound(playerSelection, computerSelection);
-    }
-
-    console.log(`Your score is: ${playerScore}, computers score is: ${computerScore}.`);
-    
-    if(playerScore > computerScore){
-        console.log("You win!")
-    } else if (playerScore < computerScore){
-        console.log("You lose!")
-    } else if (playerScore === computerScore){
-        console.log("It\'s a tie!")
-    } else return;
-}
-
-
-
-
-game();
